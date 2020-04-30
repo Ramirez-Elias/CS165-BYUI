857b1cda0cb1d2ddbb3d6b857f3d69ed0dd8079a
Ramirez-Elias <ram16032@byui.edu>
2020-04-29 21:51:56 -0500

    Moon Lander Skeleton Files

---
+++ Prove/assign06.cpp - MoonLander/velocity.cpp
@@ -0,0 +1,9 @@
+/*************************************************************
+* File: velocity.cpp
+* Author: Elias D Ramirez
+*
+* Description: Contains the implementations of the
+*  method bodies for the velocity class.
+*************************************************************/
+#include "velocity.h"
+
---
+++ Prove/assign06.cpp - MoonLander/velocity.h
@@ -0,0 +1,19 @@
+/*********************************************
+* Header file:
+*    velocity.h
+* Author:
+*    Elias D Ramirez
+* Summary:
+*    This module will create a velocity class that 
+*    will held the value to be passed to Point 
+*    class in order to move the lander on the screen
+*    and apply inertia to moves.
+**********************************************/
+#ifndef velocity_h
+#define velocity_h
+/*********************************************
+* Velocity Class.
+*********************************************/
+
+
+#endif
