7a14bb22b07e5bde2e65a0f3335ed25c1c0ea8ed
Ramirez-Elias <ram16032@byui.edu>
2020-05-01 23:44:50 -0500

    Finalized CheckPoint B: Pointers and Dynamic Allocation

--- Prepare/check07b.cpp/check07b.cpp
+++ Prepare/check07b.cpp/check07b.cpp
@@ -28,37 +28,37 @@ int main()
 	cout << "Starting Program.\n";
 
 	// 1. Add code here to declare a World
-
+	World *pWorld;
+	pWorld = new World;
 
 	// 2. Add code here to call the world's display function
+	pWorld->display();
 
 	// See page 70 in the book for details on how to access methods
 	// in pointers.
-
 	// (*newWorld).display(); // dot notation method with parenthesis operators
-
 	/* You CANNOT just do this:
 	World w;
 	w.display();
 	*/
 
 	// First, we create a voxel for air
-
+	Voxel air(180, 200, 255, 'A');
 
 	// 3. Call setBox on your world to put the air at spot (50, 60, 70)
-
+	pWorld->setBox(56, 60, 70, air);
 
 	// Next, we create a voxel for grass
-
+	Voxel grass(25, 255, 50, 'G');
 
 	// 4. Call setBox on your world to put the grass at spot (45, 20, 10)
-
+	pWorld->setBox(45, 20, 10, grass);
 
 	// 5. Call world's displayBox twice, once for each of these spots
-
-
+	pWorld->displayBox(56, 60, 70);
+	pWorld->displayBox(45, 20, 10);
 	// 6. Finally, make sure to free up your memory by deleting the world
-
+	delete pWorld;
 
 	cout << "Ending Program.\n";
 	return 0;
