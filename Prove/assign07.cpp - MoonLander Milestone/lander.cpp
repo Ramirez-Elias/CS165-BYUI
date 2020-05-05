f2c4647de59af16d0e95f09dca4548ba9748c6a7
Ramirez-Elias <ram16032@byui.edu>
2020-05-04 23:16:42 -0500

    Added functionality to Lander, including thrust left, right and up, plus gravity

---
+++ Prove/assign07.cpp - MoonLander Milestone/lander.cpp
@@ -0,0 +1,87 @@
+/***********************************************************************
+ * Implementation File:
+ *    LANDER : A class representing the lunar landscape
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    Everything you needed to know about the lander but were afraid to ask
+ ************************************************************************/
+
+#include "lander.h"
+
+
+Point Lander::getPoint() const
+{
+	return this->position;
+}
+Velocity Lander::getVelocity() const
+{
+	return this->velocity;
+}
+bool Lander::isAlive()
+{
+	return this->m_isAlive;
+}
+bool Lander::isLanded()
+{
+	return this->m_isLanded;
+}
+int Lander::getFuel()
+{
+	return this->m_fuel;
+}
+bool Lander::canThrust()
+{
+	return this->m_canTrust;
+}
+void Lander::setLanded(bool isLanded)
+{
+	this->m_isLanded = isLanded;
+}
+void Lander::setAlive(bool isAlive)
+{
+	this->m_isAlive = isAlive;
+}
+void Lander::setFuel(int fuel)
+{
+	this->m_fuel = fuel;
+}
+void Lander::applyGravity(float gravity)
+{
+	//Gravity on the moon can be modeled as 0.1 pixels per frame.
+	position.setY(position.getY() - 0.1);
+	
+	
+
+}
+void Lander::applyThrustLeft()
+{
+	//The left and right thrust amounts are 0.1 pixels per frame, and consume 1 unit of fuel.
+	velocity.setDx(velocity.getDx() + 0.1);
+	position.setX(position.getX() + 0.1);
+	setFuel(getFuel() - 1);
+}
+void Lander::applyThrustRight()
+{
+	//The left and right thrust amounts are 0.1 pixels per frame, and consume 1 unit of fuel.
+	position.setX(position.getX() - 0.1);
+	setFuel(getFuel() - 1);
+}
+void Lander::applyThrustBottom()
+{
+	//The upward thrust (caused by the down arrow) amount is 0.3 pixels per frame, and consumes 3 units of fuel.
+	position.setY(position.getY() + 0.3);
+	setFuel(getFuel() - 3);
+}
+void Lander::advance()
+{
+
+}
+void Lander::draw()
+{
+	//Requierements
+	//3- You need to fill in the draw method for the Lander class to draw it on the 
+	// screen(You don't have to draw it at the "correct" location at this point, 
+	//just somewhere on the screen).
+	drawLander(position);
+}
---
+++ Prove/assign07.cpp - MoonLander Milestone/lander.h
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Header File:
+ *    Lander : The representation of a position on the screen
+ * Author:
+ *    
+ * Summary:
+ *    Everything we need to know about a location on the screen, including
+ *    the location and the bounds.
+ ************************************************************************/
+
+
+#ifndef LANDER_H
+#define LANDER_H
+#include "velocity.h"
+#include "point.h"
+#include "uiDraw.h"
+#include <iostream>
+
+ /*********************************************
+  * LANDER
+  * A single position.
+  *********************************************/
+class Lander
+{
+private:
+	Point position;
+	Velocity velocity;
+	bool m_isAlive;
+	bool m_isLanded;
+	int m_fuel;
+	bool m_canTrust;
+	float m_gravity;
+public:
+	Lander(): position(), velocity(), m_isAlive(true), m_isLanded(false), m_fuel (500), m_canTrust(true) {};
+	Point getPoint() const;
+	Velocity getVelocity() const;
+	bool isAlive();
+	bool isLanded();
+	int getFuel();
+	bool canThrust();
+	void setLanded(bool isLanded);
+	void setAlive(bool isAlive);
+	void setFuel(int fuel);
+	void applyGravity(float gravity);
+	void applyThrustLeft();
+	void applyThrustRight();
+	void applyThrustBottom();
+	void advance();
+	void draw();
+};
+
+// stream I/O useful for debugging
+std::ostream & operator << (std::ostream & out, const Lander & pt);
+std::istream & operator >> (std::istream & in, Lander & pt);
+
+#endif // LANDER_H
